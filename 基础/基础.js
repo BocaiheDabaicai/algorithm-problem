97e52e451db8cc3fe98d5378b37d021c0119d512
Sonder <108516200+BocaiheDabaicai@users.noreply.github.com>
2023-12-04 14:53:17 +0800

    数组完结

--- 基础/基础.js
+++ 基础/基础.js
@@ -1,1 +1,22 @@
 var aaa = 1;
+
+var moveZeroes = function(nums) {
+    let count = 0;
+    nums = nums.filter(item => {
+        if(item === 0) {
+            count ++
+            return false
+        }else
+            return true
+    })
+
+    while(count>0){
+        nums.push(0)
+        count--
+    }
+
+    return nums
+};
+
+moveZeroes([0,1,0,3,12])
+
